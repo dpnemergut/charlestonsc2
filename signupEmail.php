2544a92d32574f177888d01f38ada7cce3bc5bcd
Daniel <dpnemergut@gmail.com>
2013-02-07 15:48:24 -0500

    Deleted src folder, got php script to work
    Uploaded the files to the server for the first time.
    Changed charlestonsc2.html to the standard home page index.html.
    Updated the PHP script, a person can now sign up and an email will be
    sent to me. Still want to update this to return to the signup page.

--- signupEmail.php
+++ signupEmail.php
@@ -3,14 +3,13 @@
 	$characterCode = $_POST['characterCode'];
 	$race = $_POST['race'];
 	$email = $_POST['email'];
-	$from = 'From: CharlestonSC2';
 	$to = 'dpnemergut@gmail.com';
 	$subject = 'Tournament Signup';
 	
-	$body = "New signup for $email:\n $userName\n $characterCode\n $race\n"
+	$body = "New signup for $email:\n $userName\n $characterCode\n $race\n";
 	
-	if ($_POST['submit']) {
-		if (mail ($to, $subject, $body, $from)) {
+	if (isset($_POST['submit'])) {
+		if (mail ($to, $subject, $body)) {
 			echo '<p>Thanks for registering! Expect a confirmation email soon.</p>';
 		} else {
 			echo '<p>Something terrible has happened, the internet may have exploded. Please try again.</p>';
