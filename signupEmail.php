05c39087588c0484cb04b00bc6134f6b293d194a
Daniel Nemergut <dpnemergut@gmail.com>
2013-02-11 19:22:37 -0500

    Created registration confirmation pages
    Created both a registration confirmation page as well as a registration
    error page.
    Fixed up wording in the footer, and on the home page.
    Created a generic tournament page to use in the future.

--- signupEmail.php
+++ signupEmail.php
@@ -3,16 +3,17 @@
 	$characterCode = $_POST['characterCode'];
 	$race = $_POST['race'];
 	$email = $_POST['email'];
+	$bar = $_POST['bar'];
 	$to = 'dpnemergut@gmail.com';
 	$subject = 'Tournament Signup';
 	
-	$body = "New signup for $email:\n $userName\n $characterCode\n $race\n";
+	$body = "New signup for $email:\n $userName\n $characterCode\n $race\n $bar\n";
 	
 	if (isset($_POST['submit'])) {
 		if (mail ($to, $subject, $body)) {
-			echo '<p>Thanks for registering! Expect a confirmation email soon.</p>';
+			echo "<script>window.location = 'http://www.charlestonsc2.com/registery.html'</script>";
 		} else {
-			echo '<p>Something terrible has happened, the internet may have exploded. Please try again.</p>';
+			echo "<script>window.location = 'http://www.charlestonsc2.com/registern.html'</script>";
 		}
 	}
 ?>
